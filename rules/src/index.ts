d9f489cb8eab9319fd2bf8cf23633affc6a842f9
Isilud <jolan.houot@gmail.com>
2025-04-07 14:01:26 +0200

    [TG-2 #ready-for-test] Fix: template renaming

--- rules/src/index.ts
+++ rules/src/index.ts
@@ -1,3 +1,3 @@
-export { GameTemplateRules } from './GameTemplateRules'
-export { GameTemplateOptionsSpec } from './GameTemplateOptions'
-export { GameTemplateSetup } from './GameTemplateSetup'
+export { AllStarDraftRules } from './AllStarDraftRules'
+export { AllStarDraftOptionsSpec } from './AllStarDraftOptions'
+export { AllStarDraftSetup } from './AllStarDraftSetup'
