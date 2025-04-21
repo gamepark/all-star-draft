77c9ce4a766e2aa6edf756a046ef9fb560bbbfc9
Isilud <jolan.houot@gmail.com>
2025-04-21 14:50:24 +0200

    [TG-4 #ready-for-test] feat: add game options for regular season

--- rules/src/AllStarDraftOptions.ts
+++ rules/src/AllStarDraftOptions.ts
@@ -1,5 +1,7 @@
 import { OptionsSpec } from '@gamepark/rules-api'
 import { PlayerColor, playerColors } from './PlayerColor'
+import { regularSeasonGameMode, RegularSeasonGameMode } from './RegularSeasonGameMode'
+import { TFunction } from 'i18next'
 
 /**
  * This is the options for each player in the game.
@@ -12,6 +14,7 @@ type PlayerOptions = { id: PlayerColor }
  */
 export type AllStarDraftOptions = {
   players: PlayerOptions[]
+  gameMode: RegularSeasonGameMode
 }
 
 /**
@@ -25,5 +28,35 @@ export const AllStarDraftOptionsSpec: OptionsSpec<AllStarDraftOptions> = {
       values: playerColors,
       valueSpec: (id) => ({ label: (t) => t(`player.${id}`) })
     }
+  },
+  gameMode: {
+    label: (t: TFunction) => t('option.regularSeason.mode'),
+    values: regularSeasonGameMode,
+    valueSpec: (mode) => ({
+      label: (t) => getRegularSeasonGameModeLabel(mode, t),
+      help: (t) => getRegularSeasonGameModeHelp(mode, t)
+    })
+  }
+}
+
+export function getRegularSeasonGameModeLabel(mode: RegularSeasonGameMode, t: TFunction) {
+  switch (mode) {
+    case RegularSeasonGameMode.Heritage:
+      return t('option.regularSeason.heritage.label')
+    case RegularSeasonGameMode.OpenMarket:
+      return t('option.regularSeason.openMarket.label')
+    case RegularSeasonGameMode.Duel:
+      return t('option.regularSeason.duel.label')
+  }
+}
+
+export function getRegularSeasonGameModeHelp(mode: RegularSeasonGameMode, t: TFunction) {
+  switch (mode) {
+    case RegularSeasonGameMode.Heritage:
+      return t('option.regularSeason.heritage.help')
+    case RegularSeasonGameMode.OpenMarket:
+      return t('option.regularSeason.openMarket.help')
+    case RegularSeasonGameMode.Duel:
+      return t('option.regularSeason.duel.help')
   }
 }
