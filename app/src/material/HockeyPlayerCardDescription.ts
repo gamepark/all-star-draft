a61cbdf9d6f84b4633762ec4df74f870707760f9
Isilud <jolan.houot@gmail.com>
2025-04-23 19:53:29 +0200

    feat: add hockey player card materials

---
+++ app/src/material/HockeyPlayerCardDescription.ts
@@ -0,0 +1,231 @@
+import { LocationType } from '@gamepark/all-star-draft/material/LocationType'
+import { MaterialType } from '@gamepark/all-star-draft/material/MaterialType'
+import { HockeyPlayerCard } from '@gamepark/all-star-draft/material/HockeyPlayerCard'
+import { CardDescription } from '@gamepark/react-game'
+import Beaver1 from '../images/Cards/Hockeyer/Beaver1.jpg'
+import Beaver2 from '../images/Cards/Hockeyer/Beaver2.jpg'
+import Beaver3 from '../images/Cards/Hockeyer/Beaver3.jpg'
+import Beaver4 from '../images/Cards/Hockeyer/Beaver4.jpg'
+import Beaver5 from '../images/Cards/Hockeyer/Beaver5.jpg'
+import Beaver6 from '../images/Cards/Hockeyer/Beaver6.jpg'
+import Beaver7 from '../images/Cards/Hockeyer/Beaver7.jpg'
+import Beaver8 from '../images/Cards/Hockeyer/Beaver8.jpg'
+import Beaver9 from '../images/Cards/Hockeyer/Beaver9.jpg'
+import Duck1 from '../images/Cards/Hockeyer/Duck1.jpg'
+import Duck2 from '../images/Cards/Hockeyer/Duck2.jpg'
+import Duck3 from '../images/Cards/Hockeyer/Duck3.jpg'
+import Duck4 from '../images/Cards/Hockeyer/Duck4.jpg'
+import Duck5 from '../images/Cards/Hockeyer/Duck5.jpg'
+import Duck6 from '../images/Cards/Hockeyer/Duck6.jpg'
+import Duck7 from '../images/Cards/Hockeyer/Duck7.jpg'
+import Duck8 from '../images/Cards/Hockeyer/Duck8.jpg'
+import Duck9 from '../images/Cards/Hockeyer/Duck9.jpg'
+import Eagle1 from '../images/Cards/Hockeyer/Eagle1.jpg'
+import Eagle2 from '../images/Cards/Hockeyer/Eagle2.jpg'
+import Eagle3 from '../images/Cards/Hockeyer/Eagle3.jpg'
+import Eagle4 from '../images/Cards/Hockeyer/Eagle4.jpg'
+import Eagle5 from '../images/Cards/Hockeyer/Eagle5.jpg'
+import Eagle6 from '../images/Cards/Hockeyer/Eagle6.jpg'
+import Eagle7 from '../images/Cards/Hockeyer/Eagle7.jpg'
+import Eagle8 from '../images/Cards/Hockeyer/Eagle8.jpg'
+import Eagle9 from '../images/Cards/Hockeyer/Eagle9.jpg'
+import Horse1 from '../images/Cards/Hockeyer/Horse1.jpg'
+import Horse2 from '../images/Cards/Hockeyer/Horse2.jpg'
+import Horse3 from '../images/Cards/Hockeyer/Horse3.jpg'
+import Horse4 from '../images/Cards/Hockeyer/Horse4.jpg'
+import Horse5 from '../images/Cards/Hockeyer/Horse5.jpg'
+import Horse6 from '../images/Cards/Hockeyer/Horse6.jpg'
+import Horse7 from '../images/Cards/Hockeyer/Horse7.jpg'
+import Horse8 from '../images/Cards/Hockeyer/Horse8.jpg'
+import Horse9 from '../images/Cards/Hockeyer/Horse9.jpg'
+import Reindeer1 from '../images/Cards/Hockeyer/Reindeer1.jpg'
+import Reindeer2 from '../images/Cards/Hockeyer/Reindeer2.jpg'
+import Reindeer3 from '../images/Cards/Hockeyer/Reindeer3.jpg'
+import Reindeer4 from '../images/Cards/Hockeyer/Reindeer4.jpg'
+import Reindeer5 from '../images/Cards/Hockeyer/Reindeer5.jpg'
+import Reindeer6 from '../images/Cards/Hockeyer/Reindeer6.jpg'
+import Reindeer7 from '../images/Cards/Hockeyer/Reindeer7.jpg'
+import Reindeer8 from '../images/Cards/Hockeyer/Reindeer8.jpg'
+import Reindeer9 from '../images/Cards/Hockeyer/Reindeer9.jpg'
+import Panda1 from '../images/Cards/Hockeyer/Panda1.jpg'
+import Panda2 from '../images/Cards/Hockeyer/Panda2.jpg'
+import Panda3 from '../images/Cards/Hockeyer/Panda3.jpg'
+import Panda4 from '../images/Cards/Hockeyer/Panda4.jpg'
+import Panda5 from '../images/Cards/Hockeyer/Panda5.jpg'
+import Panda6 from '../images/Cards/Hockeyer/Panda6.jpg'
+import Panda7 from '../images/Cards/Hockeyer/Panda7.jpg'
+import Panda8 from '../images/Cards/Hockeyer/Panda8.jpg'
+import Panda9 from '../images/Cards/Hockeyer/Panda9.jpg'
+import Penguin1 from '../images/Cards/Hockeyer/Penguin1.jpg'
+import Penguin2 from '../images/Cards/Hockeyer/Penguin2.jpg'
+import Penguin3 from '../images/Cards/Hockeyer/Penguin3.jpg'
+import Penguin4 from '../images/Cards/Hockeyer/Penguin4.jpg'
+import Penguin5 from '../images/Cards/Hockeyer/Penguin5.jpg'
+import Penguin6 from '../images/Cards/Hockeyer/Penguin6.jpg'
+import Penguin7 from '../images/Cards/Hockeyer/Penguin7.jpg'
+import Penguin8 from '../images/Cards/Hockeyer/Penguin8.jpg'
+import Penguin9 from '../images/Cards/Hockeyer/Penguin9.jpg'
+import PolarBear1 from '../images/Cards/Hockeyer/PolarBear1.jpg'
+import PolarBear2 from '../images/Cards/Hockeyer/PolarBear2.jpg'
+import PolarBear3 from '../images/Cards/Hockeyer/PolarBear3.jpg'
+import PolarBear4 from '../images/Cards/Hockeyer/PolarBear4.jpg'
+import PolarBear5 from '../images/Cards/Hockeyer/PolarBear5.jpg'
+import PolarBear6 from '../images/Cards/Hockeyer/PolarBear6.jpg'
+import PolarBear7 from '../images/Cards/Hockeyer/PolarBear7.jpg'
+import PolarBear8 from '../images/Cards/Hockeyer/PolarBear8.jpg'
+import PolarBear9 from '../images/Cards/Hockeyer/PolarBear9.jpg'
+import Rabbit1 from '../images/Cards/Hockeyer/Rabbit1.jpg'
+import Rabbit2 from '../images/Cards/Hockeyer/Rabbit2.jpg'
+import Rabbit3 from '../images/Cards/Hockeyer/Rabbit3.jpg'
+import Rabbit4 from '../images/Cards/Hockeyer/Rabbit4.jpg'
+import Rabbit5 from '../images/Cards/Hockeyer/Rabbit5.jpg'
+import Rabbit6 from '../images/Cards/Hockeyer/Rabbit6.jpg'
+import Rabbit7 from '../images/Cards/Hockeyer/Rabbit7.jpg'
+import Rabbit8 from '../images/Cards/Hockeyer/Rabbit8.jpg'
+import Rabbit9 from '../images/Cards/Hockeyer/Rabbit9.jpg'
+import Shark1 from '../images/Cards/Hockeyer/Shark1.jpg'
+import Shark2 from '../images/Cards/Hockeyer/Shark2.jpg'
+import Shark3 from '../images/Cards/Hockeyer/Shark3.jpg'
+import Shark4 from '../images/Cards/Hockeyer/Shark4.jpg'
+import Shark5 from '../images/Cards/Hockeyer/Shark5.jpg'
+import Shark6 from '../images/Cards/Hockeyer/Shark6.jpg'
+import Shark7 from '../images/Cards/Hockeyer/Shark7.jpg'
+import Shark8 from '../images/Cards/Hockeyer/Shark8.jpg'
+import Shark9 from '../images/Cards/Hockeyer/Shark9.jpg'
+import Tiger1 from '../images/Cards/Hockeyer/Tiger1.jpg'
+import Tiger2 from '../images/Cards/Hockeyer/Tiger2.jpg'
+import Tiger3 from '../images/Cards/Hockeyer/Tiger3.jpg'
+import Tiger4 from '../images/Cards/Hockeyer/Tiger4.jpg'
+import Tiger5 from '../images/Cards/Hockeyer/Tiger5.jpg'
+import Tiger6 from '../images/Cards/Hockeyer/Tiger6.jpg'
+import Tiger7 from '../images/Cards/Hockeyer/Tiger7.jpg'
+import Tiger8 from '../images/Cards/Hockeyer/Tiger8.jpg'
+import Tiger9 from '../images/Cards/Hockeyer/Tiger9.jpg'
+import Wolf1 from '../images/Cards/Hockeyer/Wolf1.jpg'
+import Wolf2 from '../images/Cards/Hockeyer/Wolf2.jpg'
+import Wolf3 from '../images/Cards/Hockeyer/Wolf3.jpg'
+import Wolf4 from '../images/Cards/Hockeyer/Wolf4.jpg'
+import Wolf5 from '../images/Cards/Hockeyer/Wolf5.jpg'
+import Wolf6 from '../images/Cards/Hockeyer/Wolf6.jpg'
+import Wolf7 from '../images/Cards/Hockeyer/Wolf7.jpg'
+import Wolf8 from '../images/Cards/Hockeyer/Wolf8.jpg'
+import Wolf9 from '../images/Cards/Hockeyer/Wolf9.jpg'
+import HockeyPlayerCardBack from '../images/Cards/Hockeyer/HockeyPlayerCardBack.jpg'
+
+class HockeyPlayerCardDrescription extends CardDescription<number, MaterialType, LocationType, HockeyPlayerCard> {
+  height = 8.7
+  width = 5.6
+  images = {
+    [HockeyPlayerCard.Beaver1]: Beaver1,
+    [HockeyPlayerCard.Beaver2]: Beaver2,
+    [HockeyPlayerCard.Beaver3]: Beaver3,
+    [HockeyPlayerCard.Beaver4]: Beaver4,
+    [HockeyPlayerCard.Beaver5]: Beaver5,
+    [HockeyPlayerCard.Beaver6]: Beaver6,
+    [HockeyPlayerCard.Beaver7]: Beaver7,
+    [HockeyPlayerCard.Beaver8]: Beaver8,
+    [HockeyPlayerCard.Beaver9]: Beaver9,
+    [HockeyPlayerCard.Duck1]: Duck1,
+    [HockeyPlayerCard.Duck2]: Duck2,
+    [HockeyPlayerCard.Duck3]: Duck3,
+    [HockeyPlayerCard.Duck4]: Duck4,
+    [HockeyPlayerCard.Duck5]: Duck5,
+    [HockeyPlayerCard.Duck6]: Duck6,
+    [HockeyPlayerCard.Duck7]: Duck7,
+    [HockeyPlayerCard.Duck8]: Duck8,
+    [HockeyPlayerCard.Duck9]: Duck9,
+    [HockeyPlayerCard.Eagle1]: Eagle1,
+    [HockeyPlayerCard.Eagle2]: Eagle2,
+    [HockeyPlayerCard.Eagle3]: Eagle3,
+    [HockeyPlayerCard.Eagle4]: Eagle4,
+    [HockeyPlayerCard.Eagle5]: Eagle5,
+    [HockeyPlayerCard.Eagle6]: Eagle6,
+    [HockeyPlayerCard.Eagle7]: Eagle7,
+    [HockeyPlayerCard.Eagle8]: Eagle8,
+    [HockeyPlayerCard.Eagle9]: Eagle9,
+    [HockeyPlayerCard.Horse1]: Horse1,
+    [HockeyPlayerCard.Horse2]: Horse2,
+    [HockeyPlayerCard.Horse3]: Horse3,
+    [HockeyPlayerCard.Horse4]: Horse4,
+    [HockeyPlayerCard.Horse5]: Horse5,
+    [HockeyPlayerCard.Horse6]: Horse6,
+    [HockeyPlayerCard.Horse7]: Horse7,
+    [HockeyPlayerCard.Horse8]: Horse8,
+    [HockeyPlayerCard.Horse9]: Horse9,
+    [HockeyPlayerCard.Reindeer1]: Reindeer1,
+    [HockeyPlayerCard.Reindeer2]: Reindeer2,
+    [HockeyPlayerCard.Reindeer3]: Reindeer3,
+    [HockeyPlayerCard.Reindeer4]: Reindeer4,
+    [HockeyPlayerCard.Reindeer5]: Reindeer5,
+    [HockeyPlayerCard.Reindeer6]: Reindeer6,
+    [HockeyPlayerCard.Reindeer7]: Reindeer7,
+    [HockeyPlayerCard.Reindeer8]: Reindeer8,
+    [HockeyPlayerCard.Reindeer9]: Reindeer9,
+    [HockeyPlayerCard.Panda1]: Panda1,
+    [HockeyPlayerCard.Panda2]: Panda2,
+    [HockeyPlayerCard.Panda3]: Panda3,
+    [HockeyPlayerCard.Panda4]: Panda4,
+    [HockeyPlayerCard.Panda5]: Panda5,
+    [HockeyPlayerCard.Panda6]: Panda6,
+    [HockeyPlayerCard.Panda7]: Panda7,
+    [HockeyPlayerCard.Panda8]: Panda8,
+    [HockeyPlayerCard.Panda9]: Panda9,
+    [HockeyPlayerCard.Penguin1]: Penguin1,
+    [HockeyPlayerCard.Penguin2]: Penguin2,
+    [HockeyPlayerCard.Penguin3]: Penguin3,
+    [HockeyPlayerCard.Penguin4]: Penguin4,
+    [HockeyPlayerCard.Penguin5]: Penguin5,
+    [HockeyPlayerCard.Penguin6]: Penguin6,
+    [HockeyPlayerCard.Penguin7]: Penguin7,
+    [HockeyPlayerCard.Penguin8]: Penguin8,
+    [HockeyPlayerCard.Penguin9]: Penguin9,
+    [HockeyPlayerCard.PolarBear1]: PolarBear1,
+    [HockeyPlayerCard.PolarBear2]: PolarBear2,
+    [HockeyPlayerCard.PolarBear3]: PolarBear3,
+    [HockeyPlayerCard.PolarBear4]: PolarBear4,
+    [HockeyPlayerCard.PolarBear5]: PolarBear5,
+    [HockeyPlayerCard.PolarBear6]: PolarBear6,
+    [HockeyPlayerCard.PolarBear7]: PolarBear7,
+    [HockeyPlayerCard.PolarBear8]: PolarBear8,
+    [HockeyPlayerCard.PolarBear9]: PolarBear9,
+    [HockeyPlayerCard.Rabbit1]: Rabbit1,
+    [HockeyPlayerCard.Rabbit2]: Rabbit2,
+    [HockeyPlayerCard.Rabbit3]: Rabbit3,
+    [HockeyPlayerCard.Rabbit4]: Rabbit4,
+    [HockeyPlayerCard.Rabbit5]: Rabbit5,
+    [HockeyPlayerCard.Rabbit6]: Rabbit6,
+    [HockeyPlayerCard.Rabbit7]: Rabbit7,
+    [HockeyPlayerCard.Rabbit8]: Rabbit8,
+    [HockeyPlayerCard.Rabbit9]: Rabbit9,
+    [HockeyPlayerCard.Shark1]: Shark1,
+    [HockeyPlayerCard.Shark2]: Shark2,
+    [HockeyPlayerCard.Shark3]: Shark3,
+    [HockeyPlayerCard.Shark4]: Shark4,
+    [HockeyPlayerCard.Shark5]: Shark5,
+    [HockeyPlayerCard.Shark6]: Shark6,
+    [HockeyPlayerCard.Shark7]: Shark7,
+    [HockeyPlayerCard.Shark8]: Shark8,
+    [HockeyPlayerCard.Shark9]: Shark9,
+    [HockeyPlayerCard.Tiger1]: Tiger1,
+    [HockeyPlayerCard.Tiger2]: Tiger2,
+    [HockeyPlayerCard.Tiger3]: Tiger3,
+    [HockeyPlayerCard.Tiger4]: Tiger4,
+    [HockeyPlayerCard.Tiger5]: Tiger5,
+    [HockeyPlayerCard.Tiger6]: Tiger6,
+    [HockeyPlayerCard.Tiger7]: Tiger7,
+    [HockeyPlayerCard.Tiger8]: Tiger8,
+    [HockeyPlayerCard.Tiger9]: Tiger9,
+    [HockeyPlayerCard.Wolf1]: Wolf1,
+    [HockeyPlayerCard.Wolf2]: Wolf2,
+    [HockeyPlayerCard.Wolf3]: Wolf3,
+    [HockeyPlayerCard.Wolf4]: Wolf4,
+    [HockeyPlayerCard.Wolf5]: Wolf5,
+    [HockeyPlayerCard.Wolf6]: Wolf6,
+    [HockeyPlayerCard.Wolf7]: Wolf7,
+    [HockeyPlayerCard.Wolf8]: Wolf8,
+    [HockeyPlayerCard.Wolf9]: Wolf9
+  }
+  backImage = HockeyPlayerCardBack
+}
+
+export const hockeyPlayerCardDrescription = new HockeyPlayerCardDrescription()
--- app/src/material/Material.ts
+++ app/src/material/Material.ts
@@ -1,4 +1,7 @@
 import { MaterialType } from '@gamepark/all-star-draft/material/MaterialType'
 import { MaterialDescription } from '@gamepark/react-game'
+import { hockeyPlayerCardDrescription } from './HockeyPlayerCardDescription'
 
-export const Material: Partial<Record<MaterialType, MaterialDescription>> = {}
+export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
+  [MaterialType.HockeyPlayerCard]: hockeyPlayerCardDrescription
+}
