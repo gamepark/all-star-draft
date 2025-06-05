aa7f85beec6ce52649dbd016144a881813d4b070
Isilud <jolan.houot@gmail.com>
2025-06-05 14:28:38 +0200

    feat : Add ranking and fan point

--- rules/src/material/ArenaCard.ts
+++ rules/src/material/ArenaCard.ts
@@ -23,3 +23,21 @@ export const arenaCards = getEnumValues(ArenaCard)
 export const isArenaForTwoPlayer = (cardId: ArenaCard): boolean => cardId !== ArenaCard.StadiumFall1 && cardId !== ArenaCard.Polarena3
 
 export const arenaCardsForTwoPlayers = arenaCards.filter((id) => isArenaForTwoPlayer(id))
+
+export const arenasFanPoints: Record<ArenaCard, number[]> = {
+  [ArenaCard.ComboStadium1]: [21, 18, 15, 12, 9, 6],
+  [ArenaCard.ComboStadium2]: [22, 20, 17, 15, 13, 11],
+  [ArenaCard.ComboStadium3]: [18, 16, 14, 12, 10],
+  [ArenaCard.DamStadium1]: [7, 10, 12, 13, 15, 18],
+  [ArenaCard.DamStadium2]: [10, 9, 7, 5, 6, 8],
+  [ArenaCard.DamStadium3]: [13, 14, 16, 13, 11, 9],
+  [ArenaCard.Polarena1]: [16, 15, 12, 11, 7, 6],
+  [ArenaCard.Polarena2]: [14, 13, 11, 8, 6, 5],
+  [ArenaCard.Polarena3]: [0, 4, 5, 7, 8, 12],
+  [ArenaCard.PuddlePark1]: [15, 13, 11, 9, 6, 3],
+  [ArenaCard.PuddlePark2]: [11, 10, 8, 5, 3, 2],
+  [ArenaCard.PuddlePark3]: [20, 17, 15, 14, 12, 9],
+  [ArenaCard.StadiumFall1]: [17, 12, 8, 5, 3, 2],
+  [ArenaCard.StadiumFall2]: [19, 18, 16, 13, 9, 4],
+  [ArenaCard.StadiumFall3]: [13, 10, 7, 5, 3, 1]
+}
