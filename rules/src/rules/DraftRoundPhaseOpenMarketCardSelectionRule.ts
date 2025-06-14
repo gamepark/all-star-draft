69638086bf652c8b556eaf7467b219cbbaa375c2
Isilud <jolan.houot@gmail.com>
2025-06-14 10:31:11 +0200

    fix : add 6 cards per round

--- rules/src/rules/DraftRoundPhaseOpenMarketCardSelectionRule.ts
+++ rules/src/rules/DraftRoundPhaseOpenMarketCardSelectionRule.ts
@@ -33,7 +33,10 @@ export class DraftRoundPhaseOpenMarketCardSelectionRule extends PlayerTurnRule<P
       const moves: MaterialMove<PlayerColor, MaterialType, LocationType>[] = []
       if (this.material(MaterialType.HockeyPlayerCard).location(LocationType.HockeyPlayerOpenMarketDraftLocator).length === 1)
         moves.push(this.material(MaterialType.HockeyPlayerCard).location(LocationType.HockeyPlayerOpenMarketDraftLocator).deleteItem())
-      if (this.material(MaterialType.HockeyPlayerCard).location(LocationType.PlayerHockeyPlayerHandSpot).length === 12)
+      if (
+        this.material(MaterialType.HockeyPlayerCard).location(LocationType.PlayerHockeyPlayerHandSpot).length ===
+        10 + 2 * this.remind<number>(Memorize.RoundNumber) // 5 card + 1 left per round for the two players
+      )
         if (this.remind(Memorize.RoundNumber) > 1) moves.push(this.startSimultaneousRule(RuleId.DraftRoundPhaseTeamExchange))
         else moves.push(this.startSimultaneousRule(RuleId.DraftRoundPhaseTeamCreation))
       else moves.push(this.startPlayerTurn(RuleId.DraftRoundPhaseOpenMarketCardSelection, this.nextPlayer))
