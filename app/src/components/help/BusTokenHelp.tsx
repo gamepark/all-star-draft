285ffc604d9bc4a25397a0ec59d06ab02338af9c
Isilud <jolan.houot@gmail.com>
2025-07-07 15:18:01 +0200

    feat : add help for bustoken

---
+++ app/src/components/help/BusTokenHelp.tsx
@@ -0,0 +1,16 @@
+import { MaterialHelpProps } from '@gamepark/react-game'
+import { MaterialType } from '@gamepark/all-star-draft/material/MaterialType'
+import { LocationType } from '@gamepark/all-star-draft/material/LocationType'
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
+export const BusTokenHelp: FC<MaterialHelpProps<number, MaterialType, LocationType>> = () => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <h2>{t('help.busToken.title')}</h2>
+      <p>{t('help.busToken.description')}</p>
+    </>
+  )
+}
