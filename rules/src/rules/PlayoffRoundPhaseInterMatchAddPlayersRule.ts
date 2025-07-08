d6f3a897d73156677d55402010eaa677a27d7cb7
Isilud <jolan.houot@gmail.com>
2025-07-08 16:06:02 +0200

    fix : missing score for ticket token lefts

--- rules/src/rules/PlayoffRoundPhaseInterMatchAddPlayersRule.ts
+++ rules/src/rules/PlayoffRoundPhaseInterMatchAddPlayersRule.ts
@@ -22,6 +22,8 @@ export class PlayoffRoundPhaseInterMatchAddPlayersRule extends SimultaneousRule<
     this.game.players.forEach((player) => {
       if (activePlayers.includes(player)) {
         if (this.material(MaterialType.HockeyPlayerCard).location(LocationType.PlayerHockeyPlayerHandSpot).player(player).getItems().length < 2) {
+          const ticketCount = this.material(MaterialType.PlayoffTicketToken).location(LocationType.PlayerPlayoffTicketTokenSpot).player(player).getQuantity()
+          this.memorize<number>(Memorize.Score, (score) => score + ticketCount * this.game.players.length, player)
           this.memorize<number>(Memorize.ScorePlayoff, playoffFanPoint[this.game.players.length][currentLowestPosition - 1], player)
           this.memorize<PlayerColor[]>(Memorize.ActivePlayers, (activePlayers) => activePlayers.filter((player) => player !== player))
           moves.push(this.endPlayerTurn(player))
