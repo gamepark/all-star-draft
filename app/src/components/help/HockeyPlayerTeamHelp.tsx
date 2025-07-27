120ba68ce655a8fcfbb2c74ffa00a616e07e12e2
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-07-28 01:36:01 +0200

    Fix: fixed team help card covering text in firefox

--- app/src/components/help/HockeyPlayerTeamHelp.tsx
+++ app/src/components/help/HockeyPlayerTeamHelp.tsx
@@ -15,6 +15,7 @@ import {
 import { LocationHelpProps, MaterialComponent, Picture, pointerCursorCss, usePlay, usePlayerName, useRules } from '@gamepark/react-game'
 import { MaterialMoveBuilder } from '@gamepark/rules-api'
 import { sortBy } from 'lodash'
+import { Fragment } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import { MedalIconComponent } from '../symbols/MedalIconComponent'
 import { TeamStrengthIconComponent } from '../symbols/TeamStrengthIconComponent'
@@ -54,90 +55,83 @@ export const HockeyPlayerTeamHelp = ({ location }: LocationHelpProps) => {
   const teamStrength = getTeamStrength(team, rules?.game.players.length ?? 0)
   const valueComponent = teamStrength.attribute.kind === AttributeKind.Number ? undefined : getValueComponent(teamStrength)
   return locationCards?.some((card) => card.id !== undefined) ? (
-    <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%', overflow: 'hidden' }}>
-      <ol
-        style={{
-          display: 'flex',
-          listStyle: 'none',
-          gap: '1.3em',
-          fontSize: '2em',
-          padding: 0,
-          marginLeft: '6em',
-          marginRight: '1em'
-        }}
-      >
-        {cards.map((card, index) => (
-          <li key={`player-${location.player}-team-${location.id}-list-item-${index}`} style={{ minWidth: '0em', width: '0em' }}>
-            <MaterialComponent
-              key={`player-${location.player}-team-${location.id}-card-item-${index}`}
-              type={MaterialType.HockeyPlayerCard}
-              itemId={card.id}
-              css={pointerCursorCss}
-              style={{ height: '9em', justifySelf: 'right', transform: card.id === undefined ? 'rotateY(180deg)' : '' }}
-              onClick={() => play(MaterialMoveBuilder.displayMaterialHelp(MaterialType.HockeyPlayerCard, card, index), { local: true })}
-            />
-          </li>
-        ))}
-      </ol>
-      <div>
-        <h2 style={{ padding: '0em 2em' }}>{t('help.hockeyPlayerTeam.title', { teamNumber: location.id ?? 0, name: playerName })}</h2>
-        <p>
-          <Trans
-            defaults={'help.hockeyPlayerTeam.strength'}
-            components={{ ...components, strengthSymbol: <TeamStrengthIconComponent strength={teamStrength.strength} /> }}
-            values={{ strength: teamStrength.strength }}
-          />
-        </p>
-        <p>
-          <Trans
-            defaults={'help.hockeyPlayerTeam.attribute'}
-            components={{
-              ...components,
-              medalIcon: <MedalIconComponent medalNumber={getAttributeKindPriority(numberOfPlayers).reverse().indexOf(teamStrength.attribute.kind) + 1} />
-            }}
-            values={{ attribute: getTeamStrengthAttributeTranslationKey(teamStrength, t).attributeKind }}
-          />
-        </p>
-        <p>
-          <Trans
-            defaults={'help.hockeyPlayerTeam.value'}
-            components={valueComponent === undefined ? components : { ...components, ...valueComponent }}
-            values={{ value: getTeamStrengthAttributeTranslationKey(teamStrength, t).attributeValue }}
-          />
-        </p>
-        {teamStrength.irregularsAttributes !== undefined && teamStrength.irregularsAttributes.length > 0 && (
-          <p style={{ verticalAlign: 'middle' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', alignItems: 'center' }}>
+      <h2 style={{ padding: '0em 2em' }}>{t('help.hockeyPlayerTeam.title', { teamNumber: location.id ?? 0, name: playerName })}</h2>
+      <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%', alignItems: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '40%', overflow: 'hidden', margin: '1em', gap: '1.3em', minWidth: '9.5em' }}>
+          {cards.map((card, index) => (
+            <div key={`player-${location.player}-team-${location.id}-list-item-${index}`} style={{ minWidth: '0em', width: '0em' }}>
+              <MaterialComponent
+                key={`player-${location.player}-team-${location.id}-card-item-${index}`}
+                type={MaterialType.HockeyPlayerCard}
+                itemId={card.id}
+                css={pointerCursorCss}
+                style={{ height: '9em', transform: card.id === undefined ? 'rotateY(180deg)' : '' }}
+                onClick={() => play(MaterialMoveBuilder.displayMaterialHelp(MaterialType.HockeyPlayerCard, card, index), { local: true })}
+              />
+            </div>
+          ))}
+        </div>
+        <div>
+          <p>
             <Trans
-              defaults={'help.hockeyPlayerTeam.additionalAttribute'}
-              components={{
-                ...components,
-                additionalAttributeListComponent: (
-                  <>
-                    {teamStrength.irregularsAttributes.map((irregularAttribute, index) => (
-                      <>
-                        <Picture
-                          src={
-                            irregularAttribute === IrregularAttribute.Straight
-                              ? straight
-                              : irregularAttribute === IrregularAttribute.FullHouse
-                                ? threeAndPair
-                                : allGear
-                          }
-                          height={50}
-                          style={{ verticalAlign: 'middle' }}
-                        />
-                        {getIrregularAttributeTranslationKey(irregularAttribute, t) + (index !== teamStrength.irregularsAttributes!.length - 1 ? ', ' : '')}
-                      </>
-                    ))}
-                  </>
-                )
-              }}
-              values={{
-                additionalAttributesCount: teamStrength.irregularsAttributes.length
-              }}
+              defaults={'help.hockeyPlayerTeam.strength'}
+              components={{ ...components, strengthSymbol: <TeamStrengthIconComponent strength={teamStrength.strength} /> }}
+              values={{ strength: teamStrength.strength }}
             />
           </p>
-        )}
+          <p>
+            <Trans
+              defaults={'help.hockeyPlayerTeam.attribute'}
+              components={{
+                ...components,
+                medalIcon: <MedalIconComponent medalNumber={getAttributeKindPriority(numberOfPlayers).reverse().indexOf(teamStrength.attribute.kind) + 1} />
+              }}
+              values={{ attribute: getTeamStrengthAttributeTranslationKey(teamStrength, t).attributeKind }}
+            />
+          </p>
+          <p>
+            <Trans
+              defaults={'help.hockeyPlayerTeam.value'}
+              components={valueComponent === undefined ? components : { ...components, ...valueComponent }}
+              values={{ value: getTeamStrengthAttributeTranslationKey(teamStrength, t).attributeValue }}
+            />
+          </p>
+          {teamStrength.irregularsAttributes !== undefined && teamStrength.irregularsAttributes.length > 0 && (
+            <p style={{ verticalAlign: 'middle' }}>
+              <Trans
+                defaults={'help.hockeyPlayerTeam.additionalAttribute'}
+                components={{
+                  ...components,
+                  additionalAttributeListComponent: (
+                    <>
+                      {teamStrength.irregularsAttributes.map((irregularAttribute, index) => (
+                        <Fragment key={`irregularAttribute-container-${index}`}>
+                          <Picture
+                            key={`irregularAttribute-picture-${index}`}
+                            src={
+                              irregularAttribute === IrregularAttribute.Straight
+                                ? straight
+                                : irregularAttribute === IrregularAttribute.FullHouse
+                                  ? threeAndPair
+                                  : allGear
+                            }
+                            height={50}
+                            style={{ verticalAlign: 'middle' }}
+                          />
+                          {getIrregularAttributeTranslationKey(irregularAttribute, t) + (index !== teamStrength.irregularsAttributes!.length - 1 ? ', ' : '')}
+                        </Fragment>
+                      ))}
+                    </>
+                  )
+                }}
+                values={{
+                  additionalAttributesCount: teamStrength.irregularsAttributes.length
+                }}
+              />
+            </p>
+          )}
+        </div>
       </div>
     </div>
   ) : (
