4743bfdad0dce3eba9fec13cae2106d37c05439f
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-08-06 22:56:45 +0200

    Fix: fix discarded card history component

--- app/src/components/history/common/CardDiscardedComponent.tsx
+++ app/src/components/history/common/CardDiscardedComponent.tsx
@@ -5,13 +5,13 @@ import { MaterialType } from '@gamepark/all-star-draft/material/MaterialType'
 import { PlayerColor } from '@gamepark/all-star-draft/PlayerColor'
 import { RuleId } from '@gamepark/all-star-draft/rules/RuleId'
 import { MoveComponentContext, MoveComponentProps, usePlayerName } from '@gamepark/react-game'
-import { isMoveItemType, Material, MaterialGame, MaterialMove } from '@gamepark/rules-api'
+import { isDeleteItemType, Material, MaterialGame, MaterialMove } from '@gamepark/rules-api'
 import { FC } from 'react'
 import { Trans } from 'react-i18next'
 import { CardValueLogComponent } from '../util/CardValueLogComponent'
 
 export const CardDiscardedComponent: FC<MoveComponentProps<MaterialMove<PlayerColor, MaterialType, LocationType>, PlayerColor>> = ({ move, context }) => {
-  if (!isMoveItemType<PlayerColor, MaterialType, LocationType>(MaterialType.HockeyPlayerCard)(move)) {
+  if (!isDeleteItemType<PlayerColor, MaterialType, LocationType>(MaterialType.HockeyPlayerCard)(move)) {
     return <></>
   }
   const gameContext = context as MoveComponentContext<
@@ -22,7 +22,7 @@ export const CardDiscardedComponent: FC<MoveComponentProps<MaterialMove<PlayerCo
   const discardedCard = new Material(MaterialType.HockeyPlayerCard, gameContext.game.items[MaterialType.HockeyPlayerCard])
     .index(move.itemIndex)
     .getItem<HockeyPlayerCard>()
-  const playerName = usePlayerName(move.location.player)
+  const playerName = usePlayerName(discardedCard!.location.player)
   return (
     <Trans
       defaults="history.draftPhase.cardDiscarded"
